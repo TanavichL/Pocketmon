dd6b672ea1b2cf9e0f13247a74fa43c3ecee6cc6
mindeiei <73679335+TanavichL@users.noreply.github.com>
2023-04-24 02:03:40 +0700

    signin fixx

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -8,13 +8,13 @@ function SignIn() {
     <div id='bg-login' className="h-screen bg-[#DFF2E8]">
       <NavigationBar />    
       <div className='flex justify-center items-center h-full'>
-        <div className='relative w-[33%] bg-white rounded-[15px] px-20 py-16 shadow-king'>
+        <div className='relative w-[33%] bg-white rounded-[15px] 2xl:px-20 lg:px-10 2xl:py-16 lg:py-11 shadow-king'>
             <img className='absolute w-[26vw] z-10 right-[-22rem] top-[3rem]' src={airplane} alt="" />
             <p className='font-medium text-[1.5625vw] font-lexend'>Hi, Welcome back!</p>
             <p className='text-[#C7C3C3] mt-1 text-[0.9375vw] font-lexend'>Login to Pocketmon</p>
             <form className='lg:mt-5 2xl:mt-10' action="">
-                <input className='w-full h-[3.7122969837587005vh] pl-4 border border-[#B4B4B4] border-1 rounded-[10px] outline-none text-[0.78125vw]' placeholder='Email' type="email" />
-                <input className='mt-6 w-full h-[3.7122969837587005vh] pl-4 border border-[#B4B4B4] border-1 rounded-[10px] outline-none text-[0.78125vw]' placeholder='Password' type="password" />
+                <input className='w-full h-[3.8vh] pl-4 border border-[#B4B4B4] border-1 rounded-[10px] outline-none text-[0.78125vw]' placeholder='Email' type="email" />
+                <input className='2xl:mt-6 lg:mt-4 w-full h-[3.8vh] pl-4 border border-[#B4B4B4] border-1 rounded-[10px] outline-none text-[0.78125vw]' placeholder='Password' type="password" />
             </form>
             <div className='w-full flex flex-col items-end mt-4 font-[700] text-[0.703125vw]'>
                 <a href='' className='text-[#7A7A7A] hover:text-[#07636B] duration-200 font-jura'>Forgot my password</a>
