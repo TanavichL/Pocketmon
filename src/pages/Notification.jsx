d60e366a3fe5840d649b2e887c55a06c6b7f2d56
mindeiei <73679335+TanavichL@users.noreply.github.com>
2023-05-01 22:41:39 +0700

    notification finish

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -24,9 +24,12 @@ function Notification() {
                     // setStatement(element.cloud_statement)
                     // console.log(element.cloud_statement);
                 });
+                let state2 = []
+                state2.push(...res.data.cashbox.statement);
+                console.log(res.data.cashbox);
+                state.push(...state2)
                 // setStatement(state)
                 var statement_sort = state.sort((a,b) => new Date(b[Object.keys(b)].st_date) - new Date(a[Object.keys(a)].st_date))
-                console.log(statement_sort);
                 setStatement(statement_sort)
                 // console.log(statement);
                 // console.log(state);
@@ -37,6 +40,9 @@ function Notification() {
             }
             })
         },[])
+        function currencyFormat(num) {
+            return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+          }
   return (
     <div className="min-h-screen bg-[#F9F8F8]">
         <div className='h-full w-full flex justify-center'>
@@ -53,17 +59,17 @@ function Notification() {
                             return (
                                 <div key={index} className='w-full flex justify-between items-center border-b-[1px] border-[#D9D9D9] py-5 pr-6 2xl:px-20 lg:px-8'>
                             <div className='flex items-center space-x-6'>
-                                {res[Object.keys(res)].st_status.status === "income"?
+                                {res[Object.keys(res)].st_status.cashflow === "income"?
                                 (<img src={img_income} alt="" />)
                                 :(<img src={img_outcome} alt="" />)}
                                 <div className='flex flex-col space-y-1'>
                                     <p className='text-[0.8203125vw] font-[700] text-[#243747] font-lexend'>Money Moved Successfully</p>
-                                    <p className='text-[0.703125vw] font-[500] text-[#918F8F]'>{res[Object.keys(res)].st_date} 18:42</p>
+                                    <p className='text-[0.703125vw] font-[500] text-[#918F8F]'>{moment(res[Object.keys(res)].st_date).format('LLLL')}</p>
                                 </div>
                             </div>
-                            {res[Object.keys(res)].st_status.status === "income"?
-                            (<p className='text-[1.25vw] text-[#07636B]'>+ {res[Object.keys(res)].st_amount}</p>)
-                            :(<p className='text-[1.25vw] text-[#C85952]'>- {res[Object.keys(res)].st_amount}</p>)
+                            {res[Object.keys(res)].st_status.cashflow === "income"?
+                            (<p className='text-[1.25vw] text-[#07636B]'>+ {currencyFormat(res[Object.keys(res)].st_amount)} ฿</p>)
+                            :(<p className='text-[1.25vw] text-[#C85952]'>- {currencyFormat(res[Object.keys(res)].st_amount)} ฿</p>)
                             }
                         </div>
                             )
